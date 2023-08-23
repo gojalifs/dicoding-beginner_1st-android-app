6a8f2fbf1c0ca5d4e0e746a9928d7a20ec7a2269
gojalifs <fajarsidik1999@gmail.com>
2023-08-23 17:09:55 +0700

    refactor: uses viewbinding for view

--- intent/app/src/main/java/com/satria/androidbeginnerclass_dicoding/MainActivity.kt
+++ intent/app/src/main/java/com/satria/androidbeginnerclass_dicoding/MainActivity.kt
@@ -10,14 +10,11 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.result.contract.ActivityResultContracts
+import com.satria.androidbeginnerclass_dicoding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
-    private lateinit var edtLength: EditText
-    private lateinit var edtWidth: EditText
-    private lateinit var edtHeight: EditText
-    private lateinit var btnCalculate: Button
-    private lateinit var tvResult: TextView
     private lateinit var tvMoveResult: TextView
+    private lateinit var binding: ActivityMainBinding
 
     private val resultLauncher = registerForActivityResult(
         ActivityResultContracts.StartActivityForResult()
@@ -36,12 +33,9 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        edtLength = findViewById(R.id.edt_length)
-        edtWidth = findViewById(R.id.edt_width)
-        edtHeight = findViewById(R.id.edt_height)
-        btnCalculate = findViewById(R.id.btn_calculate)
-        tvResult = findViewById(R.id.tv_result)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         tvMoveResult = findViewById(R.id.tv_move_result)
 
         val btnMoveActivity: Button = findViewById(R.id.btn_move)
@@ -57,42 +51,42 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         btnMoveForResult.setOnClickListener(this)
 
 
-        btnCalculate.setOnClickListener {
+        binding.btnCalculate.setOnClickListener {
             if (it?.id == R.id.btn_calculate) {
-                val inputLength = edtLength.text.toString().trim()
-                val inputWidth = edtWidth.text.toString().trim()
-                val inputHeight = edtHeight.text.toString().trim()
+                val inputLength = binding.edtLength.text.toString().trim()
+                val inputWidth = binding.edtWidth.text.toString().trim()
+                val inputHeight = binding.edtHeight.text.toString().trim()
                 var isEmptyFields = false
                 if (inputLength.isEmpty()) {
                     isEmptyFields = true
-                    edtLength.error = "Field ini tidak boleh kosong"
+                    binding.edtLength.error = "Field ini tidak boleh kosong"
                 }
                 if (inputWidth.isEmpty()) {
                     isEmptyFields = true
-                    edtWidth.error = "Field ini tidak boleh kosong"
+                    binding.edtWidth.error = "Field ini tidak boleh kosong"
                 }
                 if (inputHeight.isEmpty()) {
                     isEmptyFields = true
-                    edtHeight.error = "Field ini tidak boleh kosong"
+                    binding.edtHeight.error = "Field ini tidak boleh kosong"
                 }
                 if (!isEmptyFields) {
                     val volume =
                         inputLength.toDouble() * inputWidth.toDouble() * inputHeight.toDouble()
-                    tvResult.text = volume.toString()
+                    binding.tvResult.text = volume.toString()
                 }
             }
         }
 
         if (savedInstanceState != null) {
             val result = savedInstanceState.getString(STATE_RESULT)
-            tvResult.text = result
+            binding.tvResult.text = result
         }
 
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putString(STATE_RESULT, tvResult.text.toString())
+        outState.putString(STATE_RESULT, binding.tvResult.text.toString())
     }
 
     override fun onClick(v: View?) {
