6bb1f7cc952b6d7dcc6de93be45d1c264f2925e6
gojalifs <fajarsidik1999@gmail.com>
2023-08-28 11:44:30 +0700

    added drawer app

---
+++ LatihanNavigationDrawer/app/src/main/java/com/satria/dicoding/submission/latihannavigationdrawer/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package com.satria.dicoding.submission.latihannavigationdrawer.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ LatihanNavigationDrawer/app/src/main/java/com/satria/dicoding/submission/latihannavigationdrawer/ui/subway/SubwayActivity.kt
@@ -0,0 +1,12 @@
+package com.satria.dicoding.submission.latihannavigationdrawer.ui.subway
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.satria.dicoding.submission.latihannavigationdrawer.R
+
+class SubwayActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_subway)
+    }
+}
