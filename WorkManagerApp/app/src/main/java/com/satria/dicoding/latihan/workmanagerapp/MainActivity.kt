5a4c92288c9a2f32a9c1066a5a94ca36291ca3a6
gojalifs <fajarsidik1999@gmail.com>
2023-09-19 14:38:43 +0700

    work manager app : One Time Task

---
+++ WorkManagerApp/app/src/main/java/com/satria/dicoding/latihan/workmanagerapp/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.satria.dicoding.latihan.workmanagerapp
+
+import android.Manifest
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.work.Data
+import androidx.work.OneTimeWorkRequest
+import androidx.work.WorkManager
+import com.satria.dicoding.latihan.workmanagerapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var workManager: WorkManager
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        if (isGranted) {
+            Toast.makeText(this, "Notifications permission granted", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Notifications permission rejected", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (Build.VERSION.SDK_INT >= 33) {
+            requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+        }
+
+        workManager = WorkManager.getInstance(this)
+        binding.btnOneTimeTask.setOnClickListener {
+            startOneTask()
+        }
+    }
+
+    private fun startOneTask() {
+        binding.textStatus.text = getString(R.string.status)
+        val data =
+            Data.Builder()
+                .putString(MyWorker.EXTRA_CITY, binding.editCity.text.toString())
+                .build()
+        val oneTimeWorkRequest = OneTimeWorkRequest.Builder(MyWorker::class.java)
+            .setInputData(data)
+            .build()
+        workManager.enqueue(oneTimeWorkRequest)
+        workManager.getWorkInfoByIdLiveData(oneTimeWorkRequest.id)
+            .observe(this) {
+                val status = it.state.name
+                binding.textStatus.append("\n$status")
+            }
+    }
+}
